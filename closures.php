7b8d2e53bf354abad1c3bbdb5a92f982ff0f0115
Artem Borovikov <artem.borovikov.dev@gmail.com>
2024-09-30 14:37:55 +0500

    add closures.php

---
+++ closures.php
@@ -0,0 +1,23 @@
+<?php
+
+$message = 'привет';
+
+$example = function () {
+    var_dump($message);
+};
+$example();
+
+$example = function () use ($message) {
+    var_dump($message);
+};
+$example();
+
+$message = 'мир';
+$example();
+
+$message = 'привет';
+
+$example = function () use (&$message) {
+    var_dump($message);
+};
+$example();
